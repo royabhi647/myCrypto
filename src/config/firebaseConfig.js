d9340197d6b756b0cea18863978bb48a0223079c
Abhishek Kumar <royabhi647@gmail.com>
2024-05-01 09:21:30 +0530

    :rocket: [graph-chart] - dashboard

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlCnR1WA6fURV0tHR2dFg649C_1bL1xNg",
+  authDomain: "mycrypto-8e240.firebaseapp.com",
+  projectId: "mycrypto-8e240",
+  storageBucket: "mycrypto-8e240.appspot.com",
+  messagingSenderId: "248504359260",
+  appId: "1:248504359260:web:4b11228b5c9ccf4c1f8999",
+  measurementId: "G-J2LEFZNR4Y"
+};
+
+
+export default firebaseConfig;
